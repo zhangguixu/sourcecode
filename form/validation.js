121e12ef98608d3d52a41aa253f973243a57bb4a
zhangguixu <1447905210@qq.com>
2016-01-11 16:39:53 +0800

    修改 isEmpty;

--- form/validation.js
+++ form/validation.js
@@ -4,15 +4,13 @@
 
 //是否为空
 function isEmpty(d){
-	if(typeof d==='undefined' || d==='')
-		return true;
-
-	return false;
+	if(typeof d==='undefined')return true; //当值为undefined时，
+	return d.length===0?true:false;
 }
 
 //手机号码
 function isPhoneNumber(n){
-	if(isEmpty(n)|| n.length===0 || n.length!==11)
+	if(isEmpty(n)|| n.length!==11)
 		return false;
 	var reg= /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/;
 
